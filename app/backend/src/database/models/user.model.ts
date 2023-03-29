93f29c55efc20e4851734714c38f4d1b5a10143e
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2023-03-29 18:53:02 -0300

    requirement 07 completed

---
+++ app/backend/src/database/models/user.model.ts
@@ -0,0 +1,43 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import IUser from '../../interfaces/IUser';
+import db from '.';
+
+class User extends Model implements IUser {
+  declare id: number;
+  declare user: string;
+  declare email: string;
+  declare role: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default User;
