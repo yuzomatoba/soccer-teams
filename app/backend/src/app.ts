37ae617823c854da31650d406686e21240aa48e6
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2023-03-29 18:11:27 -0300

    requirement 03 completed

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import teamRoute from './routes/team.routes';
 
 class App {
   public app: express.Express;
@@ -9,6 +10,7 @@ class App {
     this.config();
 
     // Não remover essa rota
+    this.app.use('/teams', teamRoute);
     this.app.get('/', (req, res) => res.json({ ok: true }));
   }
 
